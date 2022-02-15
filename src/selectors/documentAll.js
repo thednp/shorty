08936e55a06f50808fd63b18d6dacd9fd8dbce56
thednp <dnp.digital@gmail.com>
2022-02-15 10:56:35 +0200

    Update documentAll.js

--- src/selectors/documentAll.js
+++ src/selectors/documentAll.js
@@ -2,7 +2,7 @@ import getElementsByTagName from './getElementsByTagName';
 
 /**
  * An `HTMLCollection` with all document elements,
- * which is the equivalent to `document.all`.
+ * which is the equivalent of `document.all`.
  */
 const documentAll = getElementsByTagName('*');
 export default documentAll;
