c122cef50933fffbdcc3f6cadd13bd0c13b442af
thednp <dnp.digital@gmail.com>
2022-08-22 04:55:01 +0300

    cleanup

--- cypress/plugins/esbuild-istanbul.ts
+++ cypress/plugins/esbuild-istanbul.ts
@@ -19,6 +19,7 @@ const sourceFilter = `${name}/${sourcePath}`;
 const instrumenter = createInstrumenter({
   compact: false,
   esModules: true,
+  // coverageGlobalScope: 'window',
   preserveComments: true,
   autoWrap: true,
 });
